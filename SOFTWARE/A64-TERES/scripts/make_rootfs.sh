e10416c79304e478136b23271e14b708be277f6f
d3v1c3nv11 <d3v1c3nv11@users.noreply.github.com>
2017-11-10 15:49:31 +0200

    defined KEY_SUSPEND

--- SOFTWARE/A64-TERES/scripts/make_rootfs.sh
+++ SOFTWARE/A64-TERES/scripts/make_rootfs.sh
@@ -357,7 +357,7 @@ EOF
 evdev:input:b0003v15BAp003C*
   KEYBOARD_KEY_7006f=brightnessdown
   KEYBOARD_KEY_70070=brightnessup
-  KEYBOARD_KEY_70074=suspend
+  KEYBOARD_KEY_70071=suspend
 EOF
 		add_platform_scripts
 		add_systemd_services
